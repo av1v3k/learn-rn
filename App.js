a3dd25af274eb727da8ed1386e6101e775f7f6a1
av1v3k <1721339+av1v3k@users.noreply.github.com>
2025-03-31 21:31:30 +0530

    view component

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        backgroundColor: "plum",
+        flex: 1,
+        justifyContent: "center",
+      }}
+    >
+      <View
+        style={{ backgroundColor: "#FFFFFF", width: 200, height: 200 }}
+      ></View>
+      <View style={{ backgroundColor: "teal", width: 200, height: 200 }}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 npx create-expo-app@latest . --template blank
 
 ### NOTE: Refer link here for more info: https://docs.expo.dev/more/create-expo/#--template
+
+# Core-Components:
+
+1. View
