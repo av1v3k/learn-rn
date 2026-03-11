a108101372ec47fc9271f71c0ff29189a67d46d8
av1v3k <1721339+av1v3k@users.noreply.github.com>
2026-03-11 21:15:04 +0530

    move flex:1 to <view> component container

--- App.js
+++ App.js
@@ -3,30 +3,23 @@ import Box from "./components/Box";
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "plum",
-        flex: 1,
-      }}
-    >
-      <Text>Flex</Text>
+    <View style={styles.container}>
+      <Box style={{ backgroundColor: "grey" }}> Box 1</Box>
+      <Box style={{ backgroundColor: "blue" }}>Box 1 </Box>
+      <Box style={{ backgroundColor: "teal" }}>Box 1 </Box>
+      <Box style={{ backgroundColor: "#ab9156" }}> Box 1</Box>
+      <Box style={{ backgroundColor: "#6b0803" }}> Box 1</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 1 </Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>Box 1 </Box>
     </View>
-    // <View style={styles.container}>
-    //   <Box style={{ backgroundColor: "grey" }}> Box 1</Box>
-    //   <Box style={{ backgroundColor: "blue" }}>Box 1 </Box>
-    //   <Box style={{ backgroundColor: "teal" }}>Box 1 </Box>
-    //   <Box style={{ backgroundColor: "#ab9156" }}> Box 1</Box>
-    //   <Box style={{ backgroundColor: "#6b0803" }}> Box 1</Box>
-    //   <Box style={{ backgroundColor: "#1c4c56" }}>Box 1 </Box>
-    //   <Box style={{ backgroundColor: "#b95f21" }}>Box 1 </Box>
-    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     marginTop: 40,
-    borderWidth: 0.5,
+    borderWidth: 5,
     borderColor: "red",
   },
 });
