67829a0b429b2ed25d98f19cb797eefc7ba650d8
av1v3k <1721339+av1v3k@users.noreply.github.com>
2026-03-11 21:47:53 +0530

    testing: flex: 1, flex: 3: flex: 1

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "grey" }}> Box 1</Box>
-      <Box style={{ backgroundColor: "blue" }}>Box 1 </Box>
-      <Box style={{ backgroundColor: "teal" }}>Box 1 </Box>
+      <Box style={{ backgroundColor: "grey", flex: 1 }}> Box 1</Box>
+      <Box style={{ backgroundColor: "blue", flex: 3 }}>Box 1 </Box>
+      <Box style={{ backgroundColor: "teal", flex: 1 }}>Box 1 </Box>
       <Box style={{ backgroundColor: "#ab9156" }}> Box 1</Box>
       <Box style={{ backgroundColor: "#6b0803" }}> Box 1</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 1 </Box>
